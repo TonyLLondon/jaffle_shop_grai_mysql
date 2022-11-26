a676a131e785b0963cd9eea361de5edfaf0f5a1e
Tony <xx@yy.com>
2022-11-26 13:04:38 +0000

    Jaffle shop grai demo running mysql

--- populate_grai.py
+++ populate_grai.py
@@ -8,4 +8,4 @@ client.set_authentication_headers("null@grai.io","super_secret")
 manifest_file = os.path.join(os.path.dirname(__file__),"target", "manifest.json")
 
 update_server_from_dbt(client, manifest_file)
-update_server_from_mysql(client, dbname='docker', user='docker', password='docker', namespace='jaffle_shop', host='localhost', port='5433')
+update_server_from_mysql(client, dbname='db', user='docker', password='docker', namespace='jaffle_shop', host='localhost', port='3306')
